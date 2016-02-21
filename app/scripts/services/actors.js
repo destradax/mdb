3fd2c77860f6f339ea73528209947cdd02b9cc6d
destradax <destradax@gmail.com>
2016-02-20 23:45:52 -0500

    Fixed movies and actor fixtures, and their relationships.

--- app/scripts/services/actors.js
+++ app/scripts/services/actors.js
@@ -7,14 +7,15 @@ mdb.service("actorsService", function () {
 	 */
 	this.getSampleActors = function () {
 		return [
-			{id: 1, firstName: "Tim", lastName: "Robbins", birthDate: new Date(), movieIds: [], imgUrl: "http://ia.media-imdb.com/images/M/MV5BMTI1OTYxNzAxOF5BMl5BanBnXkFtZTYwNTE5ODI4._V1_UY317_CR16,0,214,317_AL_.jpg"},
-			{id: 2, firstName: "Morgan", lastName: "Freeman", birthDate: new Date(), movieIds: [], imgUrl: "http://ia.media-imdb.com/images/M/MV5BMTc0MDMyMzI2OF5BMl5BanBnXkFtZTcwMzM2OTk1MQ@@._V1_UX214_CR0,0,214,317_AL_.jpg"},
-			{id: 3, firstName: "Marlon", lastName: "Brando", birthDate: new Date(), movieIds: [], imgUrl: "http://ia.media-imdb.com/images/M/MV5BMTg3MDYyMDE5OF5BMl5BanBnXkFtZTcwNjgyNTEzNA@@._V1_UY317_CR97,0,214,317_AL_.jpg"},
-			{id: 4, firstName: "Al", lastName: "Pacino", birthDate: new Date(), movieIds: [], imgUrl: "http://ia.media-imdb.com/images/M/MV5BMTQzMzg1ODAyNl5BMl5BanBnXkFtZTYwMjAxODQ1._V1_UX214_CR0,0,214,317_AL_.jpg"},
-			{id: 5, firstName: "Robert", lastName: "De Niro", birthDate: new Date(), movieIds: [], imgUrl: "http://ia.media-imdb.com/images/M/MV5BMjAwNDU3MzcyOV5BMl5BanBnXkFtZTcwMjc0MTIxMw@@._V1_UY317_CR13,0,214,317_AL_.jpg"},
-			{id: 6, firstName: "Christian", lastName: "Bale", birthDate: new Date(), movieIds: [], imgUrl: "http://ia.media-imdb.com/images/M/MV5BMTkxMzk4MjQ4MF5BMl5BanBnXkFtZTcwMzExODQxOA@@._V1_UX214_CR0,0,214,317_AL_.jpg"},
-			{id: 7, firstName: "John", lastName: "Travolta", birthDate: new Date(), movieIds: [], imgUrl: "http://ia.media-imdb.com/images/M/MV5BMTUwNjQ0ODkxN15BMl5BanBnXkFtZTcwMDc5NjQwNw@@._V1_UY317_CR11,0,214,317_AL_.jpg"},
-			{id: 8, firstName: "Samuel L.", lastName: "Jackson", birthDate: new Date(), movieIds: [], imgUrl: "http://ia.media-imdb.com/images/M/MV5BMTQ1NTQwMTYxNl5BMl5BanBnXkFtZTYwMjA1MzY1._V1_UX214_CR0,0,214,317_AL_.jpg"}
+			{id: 1, firstName: "Marlon", lastName: "Brando", birthDate: new Date(1924,3,3), movieIds: [1], imgUrl: "http://ia.media-imdb.com/images/M/MV5BMTg3MDYyMDE5OF5BMl5BanBnXkFtZTcwNjgyNTEzNA@@._V1_UY317_CR97,0,214,317_AL_.jpg"},
+			{id: 2, firstName: "Al", lastName: "Pacino", birthDate: new Date(1940,3,25), movieIds: [1,2], imgUrl: "http://ia.media-imdb.com/images/M/MV5BMTQzMzg1ODAyNl5BMl5BanBnXkFtZTYwMjAxODQ1._V1_UX214_CR0,0,214,317_AL_.jpg"},
+			{id: 3, firstName: "Robert", lastName: "De Niro", birthDate: new Date(1943,7,17), movieIds: [2], imgUrl: "http://ia.media-imdb.com/images/M/MV5BMjAwNDU3MzcyOV5BMl5BanBnXkFtZTcwMjc0MTIxMw@@._V1_UY317_CR13,0,214,317_AL_.jpg"},
+			{id: 4, firstName: "Tom", lastName: "Hanks", birthDate: new Date(1956,6,9), movieIds: [3], imgUrl: "http://ia.media-imdb.com/images/M/MV5BMTQ2MjMwNDA3Nl5BMl5BanBnXkFtZTcwMTA2NDY3NQ@@._V1_UY317_CR2,0,214,317_AL_.jpg"},
+			{id: 5, firstName: "Ashton", lastName: "Kutcher", birthDate: new Date(1978,1,7), movieIds: [4], imgUrl: "http://ia.media-imdb.com/images/M/MV5BMTM5Mjc2MzMzMl5BMl5BanBnXkFtZTcwOTA5OTIxNw@@._V1_UX214_CR0,0,214,317_AL_.jpg"},
+			{id: 6, firstName: "Brad", lastName: "Pitt", birthDate: new Date(1963,11,18), movieIds: [5], imgUrl: "http://ia.media-imdb.com/images/M/MV5BMjA1MjE2MTQ2MV5BMl5BanBnXkFtZTcwMjE5MDY0Nw@@._V1_UX214_CR0,0,214,317_AL_.jpg"},
+			{id: 7, firstName: "Bob", lastName: "Odenkirk", birthDate: new Date(1962,9,22), movieIds: [6], imgUrl: "http://ia.media-imdb.com/images/M/MV5BMjA1Njg1NzI1Ml5BMl5BanBnXkFtZTcwNTE5ODk1OA@@._V1_UY317_CR67,0,214,317_AL_.jpg"},
+			{id: 8, firstName: "Jean-Claude", lastName: "Van Damme", birthDate: new Date(1960,9,18), movieIds: [7], imgUrl: "http://ia.media-imdb.com/images/M/MV5BMTA2OTE1Njg4NjVeQTJeQWpwZ15BbWU3MDAyNjU4MDM@._V1_UY317_CR22,0,214,317_AL_.jpg"},
+			{id: 9, firstName: "Rowan", lastName: "Atkinson", birthDate: new Date(1955,0,6), movieIds: [8], imgUrl: "http://ia.media-imdb.com/images/M/MV5BMTg3NDUzOTc3MV5BMl5BanBnXkFtZTcwNjcxMDkxNw@@._V1_UY317_CR0,0,214,317_AL_.jpg"}
 		];
 	};
 
